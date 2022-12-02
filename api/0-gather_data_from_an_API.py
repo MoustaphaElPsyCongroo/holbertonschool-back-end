4e991b20f660a5f4a65980f6fde296f3a98db81d
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-12-02 13:27:39 +0100

    0 - Fix docstring

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,31 +3,31 @@
 from sys import argv
 import requests
 
-if len(argv) >= 2:
-    employee_id = argv[1]
 
-if employee_id is None:
-    exit()
+if __name__ == '__main__':
+    if len(argv) >= 2:
+        employee_id = argv[1]
 
-userId = {'userId': employee_id}
-id = {'id': employee_id}
-todos = requests.get(
-    'https://jsonplaceholder.typicode.com/todos', params=userId)
-todos = todos.json()
+    if employee_id is None:
+        exit()
 
-users = requests.get(
-    'https://jsonplaceholder.typicode.com/users', params=id)
-users = users.json()
+    userId = {'userId': employee_id}
+    id = {'id': employee_id}
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos', params=userId)
+    todos = todos.json()
 
-employee_name = users[0].get('name')
+    users = requests.get(
+        'https://jsonplaceholder.typicode.com/users', params=id)
+    users = users.json()
 
-tasks_done = [d for d in todos if d.get('completed') is True]
-tasks_done_nb = len(tasks_done)
-tasks_total = len(todos)
+    employee_name = users[0].get('name')
+    tasks_done = [d for d in todos if d.get('completed') is True]
+    tasks_done_nb = len(tasks_done)
+    tasks_total = len(todos)
 
+    print('Employee {} is done with tasks({}/{}):'
+          .format(employee_name, tasks_done_nb, tasks_total))
 
-print('Employee {} is done with tasks({}/{}):'.format(employee_name,
-      tasks_done_nb, tasks_total))
-
-for task in tasks_done:
-    print('\t {}'.format(task.get('title')))
+    for task in tasks_done:
+        print('\t {}'.format(task.get('title')))
